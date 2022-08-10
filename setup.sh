5e4af56b3d65bc267640219a8b719c164541b010
kondo <k.masa0921@gmail.com>
2022-08-10 21:40:23 +0900

    add:パッケージを更新

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
  #!/bin/bash
 
-DOT_FILES=(.tmux.conf)
+DOT_FILES=(.tmux.conf .zshrc .latexmkrc)
 SCRIPT_DIR=$(cd $(dirname ${0}) && pwd)
 
 echo $SCRIPT_DIR
 
-# for file in ${DOT_FILES[@]}
-# do
-#   ln -sfv $HOME/dotfiles/$file $HOME/$file
-# done
+for file in ${DOT_FILES[@]}
+do
+  ln -sfv $HOME/dotfiles/$file $HOME/$file
+done
