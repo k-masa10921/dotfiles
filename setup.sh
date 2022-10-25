511d86efe817bad135863ad34a90817e55aa6ba9
kondo <k.masa0921@gmail.com>
2022-10-25 15:12:22 +0900

    t

--- setup.sh
+++ setup.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-DOT_FILES=(.zshrc)
-SCRIPT_DIR=$(cd $(dirname ${0}) && pwd)
-
-for file in ${DOT_FILES[@]}; do
-  ln -sfv $SCRIPT_DIR/$file $HOME/$file
-=======
 DOT_FILES=(.tmux.conf .zshrc .latexmkrc .zprofile)
 SCRIPT_DIR=$(cd $(dirname ${0}) && pwd)
 
@@ -14,5 +7,4 @@ echo $SCRIPT_DIR
 
 for file in ${DOT_FILES[@]}; do
   ln -sfv $HOME/dotfiles/$file $HOME/$file
->>>>>>> 6f9b4e230c596de833572a4c503279304b09c49d
 done
